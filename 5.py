c1be3a7b386de849a405e9999646f0b82a78e50d
davewilsondev <39122987+davewilsondev@users.noreply.github.com>
2018-05-10 09:44:10 +0000

    Update 5.py
    This is a really slow way of doing it.

--- 5.py
+++ 5.py
@@ -1,1 +1,48 @@
 
+
+'''
+
+2520 is the smallest number that can be divided by each
+of the numbers from 1 to 10 without any remainder.
+
+ 
+
+What is the smallest positive number that is evenly
+divisible by all of the numbers from 1 to 20?
+
+'''
+
+ 
+
+num = 20
+
+ 
+
+cont = []
+
+a = 1
+
+ 
+
+while len(cont) != num:
+
+    print(a)
+
+    for i in
+range(1, num+1):
+
+        if a % i
+==0:
+
+           
+cont.append(a)
+
+        else:
+
+            cont =
+[]
+
+    a += 1
+
+print(cont)
+
