e632936b6134b9a6b6549336594c871f5a23a9b9
davewilsondev <39122987+davewilsondev@users.noreply.github.com>
2018-05-15 10:57:30 +0100

    Add files via upload

---
+++ 16.py
@@ -0,0 +1,16 @@
+"""2**15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2**1000?"""
+
+a = 2**1000
+print(a)
+b = str(a)
+c = []
+
+for i in b:
+    c.append(int(i))
+
+print(b)   
+print("-")
+print(sum(c))
+    
